15b24a93a03991c85d8a38a6f4da01d980c4a724
bovgirav <Bovgirav@yandex.ru>
2023-07-04 18:47:50 +0700

    #000 api auth

--- back/routes/api.php
+++ back/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\UserAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/login', [UserAuthController::class, 'login'])->middleware('guest');
 Route::post('/logout', [UserAuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::get(
+    '/csrf-cookie',
+    CsrfCookieController::class.'@show'
+)->middleware('web')->name('sanctum.csrf-cookie');

f511f75dc203a715607bf74e07ef2b0c8e93984c
bovgirav <Bovgirav@yandex.ru>
2023-07-04 18:32:51 +0700

    #000 api auth

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [UserAuthController::class, 'login'])->middleware('guest');
+Route::post('/logout', [UserAuthController::class, 'logout'])->middleware('auth:sanctum');

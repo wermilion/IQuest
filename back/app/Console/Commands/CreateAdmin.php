0c56548e5317a32f1de26f181552ae21ff6b67ff
vermilion <bodyagame682005@gmail.com>
2024-02-01 02:41:30 +0700

    create-admin command

---
+++ back/app/Console/Commands/CreateAdmin.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+
+class CreateAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create-admin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Создает пользователя-админа';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $name = $this->ask('What is your name?');
+        $email = $this->ask('Enter an email:');
+        $password = $this->ask('Enter a password:');
+
+        $this->validateArguments([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        User::query()->create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        $this->info('Admin created successfully');
+    }
+
+    protected function validateArguments($data)
+    {
+        $validator = Validator::make($data, [
+            'name' => 'min:1',
+            'email' => 'email',
+            'password' => 'min:8',
+        ]);
+
+        if ($validator->fails()) {
+            $this->error('The given arguments are invalid.');
+
+            collect($validator->errors()->all())
+                ->each(fn($error) => $this->line($error));
+            exit;
+        }
+
+        return $validator;
+    }
+}

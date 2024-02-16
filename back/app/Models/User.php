416da70fcdd7b97ef952c6e53a38dd98211c6b26
Alexander <apl10@tpu.ru>
2024-02-17 01:57:53 +0700

    Revert "test"
    This reverts commit 15379c755563dfd7a68576d36210b26d61eeb8cb.

--- back/app/Models/User.php
+++ back/app/Models/User.php
@@ -2,13 +2,12 @@
 
 namespace App\Models;
 
-use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements FilamentUser
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -42,9 +41,4 @@ class User extends Authenticatable implements FilamentUser
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-
-    public function canAccessFilament(): bool
-    {
-        return true;
-    }
 }

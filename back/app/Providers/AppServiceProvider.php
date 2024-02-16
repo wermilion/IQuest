71407614b6068dd0df7398a0c84766926008e8b6
Alexander <apl10@tpu.ru>
2024-02-17 01:57:52 +0700

    Revert "test"
    This reverts commit 51507867ef29396a27c94eedb35dfb1cee026b2f.

--- back/app/Providers/AppServiceProvider.php
+++ back/app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,13 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.force_https')) {
-            URL::forceScheme('https');
-        }
-
-        config([
-            'filament.favicon' => asset('cp/favicon.ico')
-        ]);
-
+        Model::unguard();
     }
 }

2c566161b1d92c91d64242f7a707b433acdbc661
Alexander <apl10@tpu.ru>
2024-02-19 21:56:21 +0700

    Revert "Revert "Merge branch 'fix' into 'main'""
    This reverts commit 0b8dcc224da221f411d651a267f656476e800a88.

--- back/app/Providers/Filament/AdminPanelProvider.php
+++ back/app/Providers/Filament/AdminPanelProvider.php
@@ -28,7 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('cp')
             ->path('cp')
-            ->login(Login::class)
+            ->login()
             ->colors([
                 'primary' => Color::Indigo,
             ])

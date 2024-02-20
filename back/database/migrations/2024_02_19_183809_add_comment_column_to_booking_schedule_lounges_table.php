0caa264066019fc18173a2a7b55c6a7f31893570
Bogdan <bodyagame682005@gmail.com>
2024-02-20 22:50:57 +0700

    comment column in booking_schedule_lounges table

--- back/database/migrations/2024_02_19_183809_add_comment_column_to_booking_schedule_lounges_table.php
+++ back/database/migrations/2024_02_19_183809_add_comment_column_to_booking_schedule_lounges_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('booking_schedule_lounges', function (Blueprint $table) {
-            $table->string('N')->nullable();
+            $table->string('comment')->nullable();
         });
     }
 

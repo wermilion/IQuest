3a2c3d1cf22a4c40416122a88460ae8ff8e079a9
Maksim1-smg <pmaks2365@gmail.com>
2024-02-22 19:18:43 +0700

    fix: link new window

--- front/src/components/footer-component.vue
+++ front/src/components/footer-component.vue
@@ -1,55 +1,63 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
 const linksSocial = ref([
   {
-    link: 'https://vk.com/iquest',
-    img: 'vk.svg',
+    link: "https://vk.com/iquest",
+    img: "vk.svg",
   },
   {
-    link: 'https://t.me/questtomsk',
-    img: 'telegram.svg',
+    link: "https://t.me/questtomsk",
+    img: "telegram.svg",
   },
   {
-    link: 'https://www.youtube.com/channel/UCiEXe3D9fy8GX9X73lpxOxA?disable_polymer=true',
-    img: 'youtube.svg',
+    link: "https://www.youtube.com/channel/UCiEXe3D9fy8GX9X73lpxOxA?disable_polymer=true",
+    img: "youtube.svg",
   },
-])
+]);
+
+const linkASAP = ref("https://asapeducation.ru/?utm_course=iquest_site");
 
 const contacts = ref([
   {
-    text: '94-29-10',
-    img: 'phone.svg',
+    text: "94-29-10",
+    img: "phone.svg",
   },
   {
-    text: 'iquest.tsk@gmail.com',
-    img: 'mail.svg',
+    text: "iquest.tsk@gmail.com",
+    img: "mail.svg",
   },
-])
+]);
 </script>
 
 <template>
   <footer class="border">
     <div class="container footer">
-      <img src="/logo/logo_full.svg" alt="logo">
+      <img src="/logo/logo_full.svg" alt="logo" />
       <div class="footer-links">
         <div class="footer-links__contacts">
           <span v-for="info in contacts" :key="info.text" class="body">
-            <img :src="`/icons/${info.img}`" :alt="info.img"> {{ info.text }}
+            <img :src="`/icons/${info.img}`" :alt="info.img" /> {{ info.text }}
           </span>
         </div>
         <div class="footer-links__social">
-          <a v-for="link in linksSocial" :key="link.link" :href="link.link">
-            <img :src="`/icons/social/${link.img}`" :alt="link.img">
+          <a
+            v-for="link in linksSocial"
+            :key="link.img"
+            target="_blank"
+            :href="link.link"
+          >
+            <img :src="`/icons/social/${link.img}`" :alt="link.img" />
           </a>
         </div>
       </div>
       <div class="footer-privacy">
-        <p class="smallFootnote">
-          Политика конфиденциальности
-        </p>
+        <p class="smallFootnote">Политика конфиденциальности</p>
         <span class="smallFootnote">
-          Разработано в <img src="/logo/asapLogo.svg" alt="asapLogo">
+          Разработано в
+          <a :href="linkASAP" target="_blank"
+            ><img src="/logo/asapLogo.svg" alt="asapLogo"
+          /></a>
         </span>
       </div>
     </div>

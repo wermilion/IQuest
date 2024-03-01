b6b2c65579ccadc366cf8ec84b185b78ffd5ce2a
vermilion <bodyagame682005@gmail.com>
2024-03-01 21:00:16 +0700

    fix

--- back/app/Providers/Filament/AdminPanelProvider.php
+++ back/app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('cp')
-            ->path('admin')
+            ->path('cp')
             ->login(Login::class)
             ->colors([
                 'primary' => Color::Indigo,

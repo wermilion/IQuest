478ed6990d8c241460b563ad1ed26fdf271d16ef
Bogdan <bodyagame682005@gmail.com>
2024-03-07 17:09:46 +0700

    fix CreateBookingRequest

--- back/app/Http/ApiV1/FrontApi/Modules/Bookings/Requests/CreateBookingRequest.php
+++ back/app/Http/ApiV1/FrontApi/Modules/Bookings/Requests/CreateBookingRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\ApiV1\FrontApi\Modules\Bookings\Requests;
 
 use App\Domain\Bookings\Enums\BookingType;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules\Enum;
 
 
@@ -15,9 +16,9 @@ class CreateBookingRequest extends FormRequest
             'booking.name' => ['required', 'string'],
             'booking.phone' => ['required', 'string'],
             'booking.type' => ['required', new Enum(BookingType::class)],
-            'booking.city_id' => ['required', 'integer', 'exists:cities,id'],
+            'booking.city_id' => ['required', 'integer', Rule::exists('cities', 'id')],
 
-            'schedule_quest.timeslot_id' => ['nullable', 'integer', 'exists:timeslots,id'],
+            'schedule_quest.timeslot_id' => ['nullable', 'integer', Rule::exists('timeslots', 'id')],
             'schedule_quest.count_participants' => ['nullable', 'integer', 'min:1'],
             'schedule_quest.final_price' => ['nullable', 'numeric', 'min:1'],
             'schedule_quest.comment' => ['nullable', 'string'],
@@ -25,7 +26,7 @@ class CreateBookingRequest extends FormRequest
             'holiday.holiday_id' => ['nullable', 'integer'],
             'holiday.package_id' => ['nullable', 'integer'],
 
-            'certificate_type_id' => ['nullable', 'integer', 'exists:certificate_types,id'],
+            'certificate_type_id' => ['nullable', 'integer', Rule::exists('certificate_types', 'id')],
         ];
     }
 }

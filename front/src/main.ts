c865090916543fa07919221d6743e88f8a36c182
Maksim1-smg <pmaks2365@gmail.com>
2024-03-09 21:08:39 +0700

    one

--- front/src/main.ts
+++ front/src/main.ts
@@ -2,6 +2,7 @@ import './assets/scss/global.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createYmaps } from 'vue-yandex-maps'
 
 import App from './app.vue'
 import router from './router'
@@ -16,5 +17,8 @@ initCustomDirectives(app)
 app.use(router)
 app.use(pinia)
 app.use(vuetify)
+app.use(createYmaps({
+  apikey: '9811b22a-e004-49b4-ac9a-4103a26f10ee',
+}))
 
 router.isReady().then(() => app.mount('#app'))
--- front/src/views/contacts.vue
+++ front/src/views/contacts.vue
@@ -1,7 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  YandexMap,
+  YandexMapControls,
+  YandexMapDefaultSchemeLayer,
+  YandexMapZoomControl,
+} from 'vue-yandex-maps'
+</script>
 
 <template>
-  <section><h1>Контакты</h1></section>
+  <section>
+    <h1>Контакты</h1>
+    <YandexMap
+      :settings="{
+        location: {
+
+          center: [84.947649, 56.484645],
+          zoom: 15,
+        },
+      }"
+      height="100vh"
+    >
+      <YandexMapDefaultSchemeLayer :settings="{ theme: 'dark' }" />
+      <YandexMapControls :settings="{ position: 'right' }">
+        <YandexMapZoomControl />
+      </YandexMapControls>
+    </YandexMap>
+  </section>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped>
+#map {
+  width: 100%;
+  height: 400px;
+}
+</style>
